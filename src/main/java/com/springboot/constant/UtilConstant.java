139359bc1fd81148b8c633a132c6d51672c3300c
mohitsharmalntinfotech <106802766+mohitsharmalntinfotech@users.noreply.github.com>
2022-06-22 13:25:44 +0530

    commit for boot  version upgrade

--- src/main/java/com/springboot/constant/UtilConstant.java
+++ src/main/java/com/springboot/constant/UtilConstant.java
@@ -5,11 +5,11 @@ public class UtilConstant {
 	public static final String PACKAGE_NAME = "package";
 	public static final String SPRINGBOOT_ANNOTATION_NAME = "@SpringBootApplication";
 	public static final String IMPORT_RESOURCE_STATMNT = "import org.springframework.context.annotation.ImportResource;";
-	public static final String OPENAPI_CMD = "java -jar openapi-generator-cli-4.3.1.jar generate -g spring -i openapi.yaml -c conf.json -o ";
-	public static final String OPENAPI_CONFIG_CLASS = "OpenAPIDocumentationConfig.java";
+	public static final String OPENAPI_CMD = "java -jar openapi-generator-cli-6.0.0.jar generate -g spring -i openapi.yaml -c conf.json -o ";
+	public static final String OPENAPI_CONFIG_CLASS = "SpringDocConfiguration.java";
 	public static final String OPENAPI_HEALTH_CLASS = "HealthApiController.java";
 	public static final String FORMATTER_CLASS = "RFC3339DateFormat.java";
-	public static final String OPENAPI_SPRINGBOOT_CLASS = "OpenAPI2SpringBoot";
+	public static final String OPENAPI_SPRINGBOOT_CLASS = "OpenApiGeneratorApplication";
 	public static final String APP_PROP = "application.properties";
 	public static final String POM_XML = "pom.xml";
 	public static final String CONFIG_FILE_FOR_APP_PROP = "config.properties";
